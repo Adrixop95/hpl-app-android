2e2971de8d8e1552fbf8c2097ec4e3a3fd8f833d
adrixop95 <adrixop95@me.com>
2018-08-08 12:41:41 +0200

    complete alpha version logic

--- HackintoshPolska/app/src/main/java/com/hackintoshpolska/adrox/drawertest/TutorialsActivity.java
+++ HackintoshPolska/app/src/main/java/com/hackintoshpolska/adrox/drawertest/TutorialsActivity.java
@@ -2,6 +2,7 @@ package com.hackintoshpolska.adrox.drawertest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class TutorialsActivity extends AppCompatActivity {
@@ -10,10 +11,13 @@ public class TutorialsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorials);
+
+        WebView WebViewTutorials = (WebView) findViewById(R.id.tutorweb);
+
+        WebSettings webSettings = WebViewTutorials.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        WebViewTutorials.loadUrl("https://poradniki.hackintosh-polska.pl");
+
     }
-
-
-    public void homeWV(){
-        WebView WebViewHome = (WebView) findViewById(R.id.tutorweb);
-        WebViewHome.loadUrl("https://poradniki-hackintosh-polska.pl");
-    }}
+}
